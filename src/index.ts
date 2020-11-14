4cec3d31089ffa042a0f2977f9c7971515a45b1d
xinhe0923 <yangxinhe0923@yahoo.com>
2020-11-14 18:26:32 +1100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+  const app = express();
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true, //keep touch on
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365,
+        httpOnly: true, //frontend cannt access cookie,
+        sameSite: "lax", //csrf
+        secure: __prod__, //cookie only works in https when this is true
+      },
+      secret: "randomsecret",
+      resave: false,
+    })
+  );
+  const appoloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }), //accissible for all resolvers
+  });
+
+  appoloServer.applyMiddleware({ app }); //create a graphQL endpoint
+
+  app.listen(4000, () => {
+    console.log("server is running on local 4000");
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
+});
---
+++ src/migrations/Migration20201113071750.ts
@@ -0,0 +1,9 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20201113071750 extends Migration {
+
+  async up(): Promise<void> {
+
+  }
+
+}
